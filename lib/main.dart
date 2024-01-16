a1de2c5145a9d33bb1d8b20bff5fb71ea75bf195
Jerrian 30 <jerrianlokal30@gmail.com>
2024-01-17 00:49:34 +0700

    Episode 7 List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,39 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp( const MyApp());
+void main(){
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final List<Color>myColor = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber];
+
+final List<Widget> myList = List.generate
+(100, 
+(index) => Text(
+    "${index+1}",
+    style: TextStyle(
+      fontSize: 40 + double.parse(index.toString()) ),
+    )
+  );
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title:const Text(
-            "Column",
-            style: TextStyle(color: Colors.white),
-          ),
+          title: const Text('List View'),
         ),
-        body:  Stack(
-          children: [
-             Container(
-              height: 400 ,
-              width: 400,
-              color: Colors.red,
-            ),
-            Container(
-              height: 300,
-              width: 300,
-              color: Colors.green,
-            ),
-            Container(
-              height: 200,
-              width: 200,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-            ),
-          ]
-        ),
-      ),
+        body: ListView(
+          children: myList,
+          // scrollDirection: Axis.horizontal,
+        ), 
+      )
     );
   }
 }
