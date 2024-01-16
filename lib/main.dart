f3c0b47f726f0f020c06dac982d74682f6e2b501
Jerrian 30 <jerrianlokal30@gmail.com>
2024-01-17 03:18:01 +0700

    Ep 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,43 @@
 import 'package:flutter/material.dart';
 
 main(){
-  runApp(const MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title : const Text("IMAGE WIDGET",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          )
+          title: const Text("Exstact WIdget"),
         ),
-        body: Center(
-          child: Container(
-            height: 500,
-            width: 300,
-            color: Colors.red,
-            child: Image.network(
-              "https://picsum.photos/200", 
-                fit: BoxFit.cover ,
-              ),
-            // child: const Image(
-              
-            //   fit: BoxFit.cover, 
-            //   image : AssetImage("images/jerot.jpg"),
-            //   // image: NetworkImage("https://picsum.photos/30/600"),
-            // ),
-          ),
+        body: ListView( 
+          children: const [
+            ChatItem(),
+            ChatItem() 
+          ],
         ),
       ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  const ChatItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile(
+      leading: CircleAvatar(),
+      title: Text("Jerrian"),
+      subtitle: Text("Hai aku Jerry"),
+      trailing: Text("10.00 PM"),
     );
   }
 }
